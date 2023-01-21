914edd05e1677a7b77a3398ae21ee6b61d10597c
William Swartzendruber <wswartzendruber@gmail.com>
2023-01-21 12:45:43 -0700

    Basics of RuneEncoder.

--- RuneEncoding/RuneEncoder.cs
+++ RuneEncoding/RuneEncoder.cs
@@ -10,9 +10,66 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+using System.Text;
+
 namespace RuneEncoding;
 
-public class RuneEncoder
+public abstract class RuneEncoder : Encoder
 {
+    private readonly SurrogateComposer ConvertComposer = new();
+    private readonly SurrogateComposer CountComposer = new();
 
+    public override int GetByteCount(char[] chars, int index, int count, bool flush)
+    {
+        var returnValue = 0;
+        var end = index + count;
+        var callback = delegate(int value)
+        {
+            returnValue += ByteCount(value);
+        };
+
+        CountComposer.Reset();
+
+        if (ConvertComposer.HighSurrogate is char highSurrogate)
+            CountComposer.Input(highSurrogate, callback);
+
+        for (int i = index; i < end; i++)
+            CountComposer.Input(chars[i], callback);
+
+        if (flush == true)
+            CountComposer.Flush(callback);
+
+        return returnValue;
+    }
+
+    public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes
+        , int byteIndex, bool flush)
+    {
+        var startByteIndex = byteIndex;
+        var charEnd = charIndex + charCount;
+        var callback = delegate(int value)
+        {
+            byteIndex += WriteBytes(value, bytes, byteIndex);
+        };
+
+        for (int i = charIndex; i < charEnd; charIndex++)
+            ConvertComposer.Input(chars[i], callback);
+
+        if (flush == true)
+            ConvertComposer.Flush(callback);
+
+        return byteIndex - startByteIndex;
+    }
+
+    protected abstract int ByteCount(int scalarValue);
+
+    protected abstract int WriteBytes(int scalarValue, byte[] bytes, int index);
+
+    public override void Reset()
+    {
+        ConvertComposer.Reset();
+        ResetState();
+    }
+
+    protected virtual void ResetState() { }
 }
